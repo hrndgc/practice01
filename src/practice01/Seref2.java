ce6e28744764dd6b3302131093b96715c30acaf0
srftuza <sereftuza@gmail.com>
2021-04-11 20:59:24 +0200

    seref2

--- src/practice01/Seref2.java
+++ src/practice01/Seref2.java
@@ -1,11 +1,9 @@
 package practice01;
 
-public class Seref {
+public class Seref2 {
 
 	public static void main(String[] args) {
-		System.out.println("Seref first  Class");
-		
-		
+		System.out.println("Seref second  Class");
 
 	}
 
